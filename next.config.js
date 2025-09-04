72e561dc1eb34957e84f2a559f57f4c5b12aa8e9
FGuerriero <fernandoguerrierocs@gmail.com>
2025-09-04 14:06:50 -0300

    - Created first Dockerfile; - Created first GitHub Workflows version; - fixed copyright date

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2025 Fernando Guerriero Cardoso da Silva.
+ * Created Date: Wed Jan 15 2025
+ * SPDX-License-Identifier: MIT
+ */
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+}
+
+module.exports = nextConfig
